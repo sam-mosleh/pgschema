292ff338e4bda0e8fccd887a53a9fa8eb82298c6
Tianzhou <t@bytebase.com>
2025-10-31 10:22:31 -0700

    fix: missing transaction_timeout comment

--- testdata/dump/issue_133_index_sort/pgdump.sql
+++ testdata/dump/issue_133_index_sort/pgdump.sql
@@ -8,7 +8,8 @@
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
-SET transaction_timeout = 0;
+-- transaction_timeout is introduced in PG 17
+-- SET transaction_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SELECT pg_catalog.set_config('search_path', '', false);
